7b9903fe76759b9d0ed58fa0e2546a9d147e8c88
Dilin-am <dilinam91@gmail.com>
2023-08-16 00:28:15 +0530

    code format

--- one-school/src/App.js
+++ one-school/src/App.js
@@ -1,22 +1,23 @@
+import "./App.css";
 
-import './App.css';
-
-import Navbar from './Componets/Navbar';
-import RightSection from './Componets/RightSection';
-import Sidebar from './Componets/Sidebar';
-import './Componets/MainContainer.css';
-
+import Navbar from "./Componets/Navbar";
+import RightSection from "./Componets/RightSection";
+import Sidebar from "./Componets/Sidebar";
+import "./Componets/MainContainer.css";
 
 function App() {
   return (
-    <div className='container-fuild'>
-      <div className='row-12'><Navbar/></div>
-      <div className='row-12'>
-        <div className="main-container"><Sidebar/><RightSection/></div>
-        <div></div>
-        
+    <div className="container-fuild">
+      <div className="row-12">
+        <Navbar />
+      </div>
+      <div className="row-12">
+        <div className="main-container">
+          <Sidebar />
+          <RightSection />
         </div>
-     
+        <div></div>
+      </div>
     </div>
   );
 }
--- one-school/src/Componets/Navbar.js
+++ one-school/src/Componets/Navbar.js
@@ -4,27 +4,46 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid" onClick={(Event) => console.log(Event.target)}>
-        <a className="navbar-brand na" href="navbar.js" >
-          <img src={logo} alt="logo" height="30" className="d-inline-block align-text-top"  />
-        One school  
+      <div
+        className="container-fluid"
+        onClick={(Event) => console.log(Event.target)}
+      >
+        <a className="navbar-brand na" href="navbar.js">
+          <img
+            src={logo}
+            alt="logo"
+            height="30"
+            className="d-inline-block align-text-top"
+          />
+          One school
         </a>
       </div>
       <div className="d-flex">
         <div className="dropdown dropstart">
-          
-          <button className="btn profile btn-circle custom-button" data-bs-toggle="dropdown">
+          <button
+            className="btn profile btn-circle custom-button"
+            data-bs-toggle="dropdown"
+          >
+            <img src={profile} alt="profile" className="img-fluid" />
+          </button>
+          <ul className="dropdown-menu text-small shadow">
+            <li>
+              <a className="dropdown-item" href="#">
+                Settings
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="#">
+                Profile
+              </a>
+            </li>
 
-        <img src={profile} alt="profile" className="img-fluid" />
-        </button>
-        <ul className="dropdown-menu text-small shadow">
-         <li><a className="dropdown-item" href="#">Settings</a></li>
-         <li><a className="dropdown-item" href="#">Profile</a></li>
-        
-         <li><a className="dropdown-item" href="#">Sign-out</a></li>
-
-
-        </ul>
+            <li>
+              <a className="dropdown-item" href="#">
+                Sign-out
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
